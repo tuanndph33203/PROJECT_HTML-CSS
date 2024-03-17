045e787c9f00c5ab9e0ac2da69716390bface0fa
Nguyen Dinh Tuan <junnguyenvizito@gmail.com>
2024-03-17 18:44:41 +0700

    muitipage app

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        shop: resolve(__dirname, 'views/shop.html'),
+        detail: resolve(__dirname, 'views/detail.html'),
+        checkout: resolve(__dirname, 'views/checkout.html'),
+        cart: resolve(__dirname, 'views/cart.html'),
+        login: resolve(__dirname, 'views/login.html'),
+      },
+    },
+  },
+})
