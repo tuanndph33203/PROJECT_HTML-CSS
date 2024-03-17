85a93408f3b34b80f5d1dca1909cacebe29baca1
Nguyen Dinh Tuan <junnguyenvizito@gmail.com>
2024-03-17 18:53:47 +0700

    fix netify

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        shop: resolve(__dirname, 'views/shop.html'),
-        detail: resolve(__dirname, 'views/detail.html'),
-        checkout: resolve(__dirname, 'views/checkout.html'),
-        cart: resolve(__dirname, 'views/cart.html'),
-        login: resolve(__dirname, 'views/login.html'),
+        shop: resolve(__dirname, './views/shop.html'),
+        detail: resolve(__dirname, './views/detail.html'),
+        checkout: resolve(__dirname, './views/checkout.html'),
+        cart: resolve(__dirname, './views/cart.html'),
+        login: resolve(__dirname, './views/login.html'),
       },
     },
   },
